8c791a17129d6281c35855a3b400354388e51027
rash <orashusedmund@gmail.com>
2024-01-01 13:10:02 +0100

    added form validation to both create and edit invoice forms. and archieved increase accesibility through this

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -1,16 +1,31 @@
-'use client'
+'use client';
 
-interface Props {};
+import { useEffect } from 'react';
 
-export default function error(props: Props) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-        <h2>
-            Error Page
-        </h2>
-        <pre>
-            {JSON.stringify(props, null, 4)}
-        </pre>
-    </div>
-  )
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Something went wrong!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
 }
